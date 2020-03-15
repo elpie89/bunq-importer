21211664473b31e7ee148575becc1a8fa7ebc568
James Cole <thegrumpydictator@gmail.com>
2020-03-15 19:49:09 +0100

    Another alignment.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -91,11 +91,12 @@ class RegisterController extends Controller
     protected function validator(array $data): \Illuminate\Contracts\Validation\Validator
     {
         return Validator::make(
-            $data, [
-                     'name'     => ['required', 'string', 'max:255'],
-                     'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
-                     'password' => ['required', 'string', 'min:8', 'confirmed'],
-                 ]
+            $data,
+            [
+                'name'     => ['required', 'string', 'max:255'],
+                'email'    => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                'password' => ['required', 'string', 'min:8', 'confirmed'],
+            ]
         );
     }
 }
