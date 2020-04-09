4a3ad92ee8fa029a61a25a4c42270a3777d9bf80
James Cole <thegrumpydictator@gmail.com>
2020-04-09 19:19:17 +0200

    Fix trait

--- app/Console/StartSync.php
+++ app/Console/StartSync.php
@@ -76,24 +76,4 @@ trait StartSync
 
         return 0;
     }
-
-    /**
-     * @param string $key
-     * @param array  $messages
-     */
-    protected function listMessages(string $key, array $messages): void
-    {
-        if (count($messages) > 0) {
-            /**
-             * @var int   $index
-             * @var array $error
-             */
-            foreach ($messages as $index => $list) {
-                /** @var string $line */
-                foreach ($list as $line) {
-                    $this->error(sprintf('%s in line #%d: %s', $key, $index + 1, $line));
-                }
-            }
-        }
-    }
 }
